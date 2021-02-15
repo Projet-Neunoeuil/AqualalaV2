6f0077fde754dba73caa2263b66931eaad681261
taconet1 <>
2021-02-15 18:08:48 +0100

    Day 1_Remake appli

--- Appli/app/src/main/java/fr/unilim/iut/aqualala/MainMenu.kt
+++ Appli/app/src/main/java/fr/unilim/iut/aqualala/MainMenu.kt
@@ -1,11 +1,20 @@
 package fr.unilim.iut.aqualala
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Bouton température */
+        val btnTemperature = findViewById<ImageButton>(R.id.imageThermo)
+        btnTemperature.setOnClickListener {
+            val intent = Intent(this@MainMenu, Temperature::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ Appli/app/src/main/java/fr/unilim/iut/aqualala/commons/DatabaseInfos.kt
@@ -0,0 +1,7 @@
+package fr.unilim.iut.aqualala.commons
+
+const val ADRESSE_DB = "xiangyu-an.fr"
+const val PORT_DB = "3306"
+const val NOM_DB = "Application"
+const val NOM_UTILISATEUR = "Appli"
+const val MDP = "#M0td3p@553"
