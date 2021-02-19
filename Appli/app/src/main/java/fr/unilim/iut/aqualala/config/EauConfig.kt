8434fd6981c429b767ce8be45da006397f77a8b8
AN-Xiang-yu <62239241+AN-Xiang-yu@users.noreply.github.com>
2021-02-19 21:55:02 +0100

    Nouveaux Configs

---
+++ Appli/app/src/main/java/fr/unilim/iut/aqualala/config/EauConfig.kt
@@ -0,0 +1,4 @@
+package fr.unilim.iut.aqualala.config
+
+const val MINIMUM_FREQUENCE_CHANGEMENT_EAU=3
+const val MAXIMUM_FREQUENCE_CHANGEMENT_EAU=7
---
+++ Appli/app/src/main/java/fr/unilim/iut/aqualala/config/TemperatureConfig.kt
@@ -0,0 +1,9 @@
+package fr.unilim.iut.aqualala.config
+
+const val PLUS_BASSE_TEMPERATURE=10
+const val PLUS_HAUTE_TEMPERATURE=50
+const val MINIMUM_RAFRACHIR_PAGE_TEMPERATURE=1
+const val MAXMUM_RAFRACHIR_PAGE_TEMPERATURE=60
+const val IDEALE = 0
+const val BASSE = 1
+const val HAUTE = 2
