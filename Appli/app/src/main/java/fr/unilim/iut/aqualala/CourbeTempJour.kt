081cd6cf5b167c3d0a2277201da79b45fa6f3d05
__Wither <wither.plugins@gmail.com>
2021-02-20 17:51:37 +0100

    fix graphiques commencés

--- Appli/app/src/main/java/fr/unilim/iut/aqualala/CourbeTempJour.kt
+++ Appli/app/src/main/java/fr/unilim/iut/aqualala/CourbeTempJour.kt
@@ -1,6 +1,6 @@
 package fr.unilim.iut.aqualala
 
-import android.content.Context
+
 import android.content.pm.ActivityInfo
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -8,57 +8,30 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.LineGraphSeries
-import fr.unilim.iut.aqualala.config.*
-import fr.unilim.iut.aqualala.model.sql.Connecteur
 import fr.unilim.iut.aqualala.model.sql.CourbesManager
-import java.sql.Connection
 import java.util.concurrent.Executors
-import kotlin.collections.ArrayList
 
 class CourbeTempJour : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) { // Si le téléphone est compatible alors
-            window.navigationBarColor = ContextCompat.getColor(this, R.color.orange); // Changer la barre du bas en orange
-            window.statusBarColor = ContextCompat.getColor(this, R.color.orange); // Changer la barre du haut en orange
+            window.navigationBarColor =
+                ContextCompat.getColor(this, R.color.orange); // Changer la barre du bas en orange
+            window.statusBarColor =
+                ContextCompat.getColor(this, R.color.orange); // Changer la barre du haut en orange
         }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_courbe_temp_jour)
-        val graph : GraphView = findViewById(R.id.graph)
-        graph.title = "Evolution de la température pendant les 24 dernières heures"
-        graph.viewport.isScalable = true
-        graph.viewport.setMaxX(24.0)
+        val graph: GraphView = findViewById(R.id.graph)
         graph.visibility = View.VISIBLE
         var exec = Executors.newSingleThreadExecutor()
         val handler = Handler(Looper.getMainLooper())
-        exec.execute{
+        exec.execute {
 
-            var listeTemp : ArrayList<DataPoint> = CourbesManager().obtenirCourbeJournee()
+            CourbesManager().obtenirCourbeJournee(graph)
 
-            handler.post {
-                var dataArr = arrayOfNulls<DataPoint>(listeTemp.size)
-                dataArr = listeTemp.toArray(dataArr)
-                var series : LineGraphSeries<DataPoint?> = LineGraphSeries(dataArr)
-                series.isDrawDataPoints = true
-                series.thickness = 8
-                series.setOnDataPointTapListener { series, dataPoint ->
-                    Toast.makeText(
-                        getMyActivity(),
-                        "La température est de ${dataPoint.y}°C",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                graph.addSeries(series)
-            }
         }
-    }
-
-    private fun getMyActivity(): Context? {
-        return this;
     }
 }
--- Appli/app/src/main/java/fr/unilim/iut/aqualala/model/sql/CourbesManager.kt
+++ Appli/app/src/main/java/fr/unilim/iut/aqualala/model/sql/CourbesManager.kt
@@ -1,38 +1,240 @@
 package fr.unilim.iut.aqualala.model.sql
 
-import android.os.Handler
-import android.os.Looper
+
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.jjoe64.graphview.DefaultLabelFormatter
+import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
-import java.sql.Connection
+import com.jjoe64.graphview.series.LineGraphSeries
+import fr.unilim.iut.aqualala.R
 import java.sql.PreparedStatement
-import java.util.concurrent.Executors
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class CourbesManager : ManagerAbstract() {
 
-    fun obtenirCourbe(sql : String) : ArrayList<DataPoint> {
-        var ps : PreparedStatement = connection.prepareStatement(sql)
+
+    fun obtenirCourbeJournee(graph: GraphView) : GraphView{
+        var ps : PreparedStatement = connection.prepareStatement("SELECT value, hour FROM LastDay ORDER BY hour ASC")
         var rs = ps.executeQuery()
         var x = 0.0
-        var listeValeurs : ArrayList<DataPoint> = ArrayList()
+        var datalist: ArrayList<DataPoint> = ArrayList()
+        var hourlist : HashMap<Double, Long> = HashMap()
+        var maxY = 0.0
         while (rs.next()) {
-            var valeur = rs.getDouble("value")
-            listeValeurs.add(DataPoint(x, valeur))
+
+            var heure: Date = rs.getTimestamp("hour")
+            var value = rs.getDouble("value")
+            datalist.add(DataPoint(x, value))
+            hourlist[x] = heure.time
+            if (value > maxY) {
+                maxY = value
+            }
             x += 1
         }
+        var dataArray = arrayOfNulls<DataPoint>(datalist.size)
+        dataArray = datalist.toArray(dataArray)
+        var series : LineGraphSeries<DataPoint> = LineGraphSeries(dataArray)
         ps.close()
-        return listeValeurs
+        val format = SimpleDateFormat("EEEE dd MMMM yyyy à HH:mm")
+        series.isDrawDataPoints = true
+        series.setOnDataPointTapListener { _, dataPoint ->
+            Toast.makeText(
+                graph.context,
+                "La température moyenne était de ${dataPoint.y}°C le ${format.format(hourlist[dataPoint.x])}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        series.color = ContextCompat.getColor(graph.context, R.color.orange)
+        graph.addSeries(series)
+        var sdf = SimpleDateFormat("EEE HH:mm")
+        graph.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+            override fun formatLabel(value: Double, isValueX: Boolean): String {
+                return if (isValueX) {
+                    sdf.format(Date(hourlist[value]!!))
+                } else {
+                    super.formatLabel(value, isValueX)
+                }
+            }
+        }
+        graph.viewport.setMinX(datalist[0].x)
+        graph.viewport.setMaxX(datalist[datalist.size - 1].x)
+        graph.viewport.setMaxY(maxY)
+        graph.viewport.maxXAxisSize = datalist.size.toDouble()
+        var numberOfPoint = datalist.size
+        if(datalist.size > 12) {
+            numberOfPoint = datalist.size / 6
+        }
+        graph.gridLabelRenderer.numHorizontalLabels = numberOfPoint
+        graph.viewport.isXAxisBoundsManual = true
+        graph.title = "Evolution de la température sur les ${datalist.size} dernières heures"
+        return graph
     }
-    fun obtenirCourbeJournee(): ArrayList<DataPoint> {
-        return obtenirCourbe("SELECT value FROM LastDay ORDER BY hour ASC")
+    fun obtenirCourbeSemaine(graph : GraphView): GraphView{
+        var ps : PreparedStatement = connection.prepareStatement("SELECT value, day FROM LastWeek ORDER BY day ASC")
+        var rs = ps.executeQuery()
+        var x = 0.0
+        var datalist: ArrayList<DataPoint> = ArrayList()
+        var hourlist : HashMap<Double, Long> = HashMap()
+        var maxY = 0.0
+        while (rs.next()) {
+
+            var heure: Date = rs.getTimestamp("day")
+            var value = rs.getDouble("value")
+            datalist.add(DataPoint(x, value))
+            hourlist[x] = heure.time
+            if (value > maxY) {
+                maxY = value
+            }
+            x += 1
+        }
+        var dataArray = arrayOfNulls<DataPoint>(datalist.size)
+        dataArray = datalist.toArray(dataArray)
+        var series : LineGraphSeries<DataPoint> = LineGraphSeries(dataArray)
+        ps.close()
+        val format = SimpleDateFormat("EEEE dd MMMM yyyy")
+        series.isDrawDataPoints = true
+        series.setOnDataPointTapListener { _, dataPoint ->
+            Toast.makeText(
+                graph.context,
+                "La température moyenne était de ${dataPoint.y}°C le ${format.format(hourlist[dataPoint.x])}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        series.color = ContextCompat.getColor(graph.context, R.color.orange)
+        graph.addSeries(series)
+        var sdf = SimpleDateFormat("dd MMM")
+        graph.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+            override fun formatLabel(value: Double, isValueX: Boolean): String {
+                return if (isValueX) {
+                    sdf.format(Date(hourlist[value]!!))
+                } else {
+                    super.formatLabel(value, isValueX)
+                }
+            }
+        }
+        graph.viewport.setMinX(datalist[0].x)
+        graph.viewport.setMaxX(datalist[datalist.size - 1].x)
+        graph.viewport.setMaxY(maxY)
+        graph.viewport.maxXAxisSize = datalist.size.toDouble()
+        graph.gridLabelRenderer.numHorizontalLabels = datalist.size
+        graph.viewport.isXAxisBoundsManual = true
+        graph.title = "Evolution de la température sur les ${datalist.size} derniers jours"
+        return graph
     }
-    fun obtenirCourbeSemaine(): ArrayList<DataPoint> {
-        return obtenirCourbe("SELECT value FROM LastWeek ORDER BY day ASC")
-    }
-    fun obtenirCourbeMois(): ArrayList<DataPoint> {
-        return obtenirCourbe("SELECT value FROM LastMonth ORDER BY day ASC")
+    fun obtenirCourbeMois(graph : GraphView): GraphView {
+        var ps : PreparedStatement = connection.prepareStatement("SELECT value, day FROM LastMonth ORDER BY day ASC")
+        var rs = ps.executeQuery()
+        var x = 0.0
+        var datalist: ArrayList<DataPoint> = ArrayList()
+        var hourlist : HashMap<Double, Long> = HashMap()
+        var maxY = 0.0
+        while (rs.next()) {
+
+            var heure: Date = rs.getTimestamp("day")
+            var value = rs.getDouble("value")
+            datalist.add(DataPoint(x, value))
+            hourlist[x] = heure.time
+            if (value > maxY) {
+                maxY = value
+            }
+            x += 1
+        }
+        var dataArray = arrayOfNulls<DataPoint>(datalist.size)
+        dataArray = datalist.toArray(dataArray)
+        var series : LineGraphSeries<DataPoint> = LineGraphSeries(dataArray)
+        ps.close()
+        val format = SimpleDateFormat("EEEE dd MMMM yyyy")
+        series.isDrawDataPoints = true
+        series.setOnDataPointTapListener { _, dataPoint ->
+            Toast.makeText(
+                graph.context,
+                "La température moyenne était de ${dataPoint.y}°C le ${format.format(hourlist[dataPoint.x])}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        series.color = ContextCompat.getColor(graph.context, R.color.orange)
+        graph.addSeries(series)
+        var sdf = SimpleDateFormat("dd MMM")
+        graph.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+            override fun formatLabel(value: Double, isValueX: Boolean): String {
+                return if (isValueX) {
+                    sdf.format(Date(hourlist[value]!!))
+                } else {
+                    super.formatLabel(value, isValueX)
+                }
+            }
+        }
+        graph.viewport.setMinX(datalist[0].x)
+        graph.viewport.setMaxX(datalist[datalist.size - 1].x)
+        graph.viewport.setMaxY(maxY)
+        graph.viewport.maxXAxisSize = datalist.size.toDouble()
+        var numberOfPoint = datalist.size
+        if(datalist.size > 30) {
+            numberOfPoint = datalist.size / 5
+        }
+        graph.gridLabelRenderer.numHorizontalLabels = numberOfPoint
+        graph.viewport.isXAxisBoundsManual = true
+        graph.title = "Evolution de la température sur les ${datalist.size} derniers jours"
+        return graph
     }
 
-    fun obtenirCourbeAnnee(): ArrayList<DataPoint> {
-        return obtenirCourbe("SELECT value FROM LastYear ORDER BY month ASC")
+    fun obtenirCourbeAnnee(graph : GraphView): GraphView {
+        var ps : PreparedStatement = connection.prepareStatement("SELECT value, month FROM LastYear ORDER BY month ASC")
+        var rs = ps.executeQuery()
+        var x = 0.0
+        var datalist: ArrayList<DataPoint> = ArrayList()
+        var hourlist : HashMap<Double, Long> = HashMap()
+        var maxY = 0.0
+        while (rs.next()) {
+
+            var heure: Date = rs.getTimestamp("month")
+            var value = rs.getDouble("value")
+            datalist.add(DataPoint(x, value))
+            hourlist[x] = heure.time
+            if (value > maxY) {
+                maxY = value
+            }
+            x += 1
+        }
+        var dataArray = arrayOfNulls<DataPoint>(datalist.size)
+        dataArray = datalist.toArray(dataArray)
+        var series : LineGraphSeries<DataPoint> = LineGraphSeries(dataArray)
+        ps.close()
+        val format = SimpleDateFormat("MMMM yyyy")
+        series.isDrawDataPoints = true
+        series.setOnDataPointTapListener { _, dataPoint ->
+            Toast.makeText(
+                graph.context,
+                "La température moyenne était de ${dataPoint.y}°C en ${format.format(hourlist[dataPoint.x])}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        series.color = ContextCompat.getColor(graph.context, R.color.orange)
+        graph.addSeries(series)
+        var sdf = SimpleDateFormat("MMM yyyy")
+        graph.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+            override fun formatLabel(value: Double, isValueX: Boolean): String {
+                return if (isValueX) {
+                    sdf.format(Date(hourlist[value]!!))
+                } else {
+                    super.formatLabel(value, isValueX)
+                }
+            }
+        }
+        graph.viewport.setMinX(datalist[0].x)
+        graph.viewport.setMaxX(datalist[datalist.size - 1].x)
+        graph.viewport.setMaxY(maxY)
+        graph.viewport.maxXAxisSize = datalist.size.toDouble()
+        var numberOfPoint = datalist.size
+        if(datalist.size > 10) {
+            numberOfPoint = datalist.size / 2
+        }
+        graph.gridLabelRenderer.numHorizontalLabels = numberOfPoint
+        graph.viewport.isXAxisBoundsManual = true
+        graph.title = "Evolution de la température sur les ${datalist.size} derniers mois"
+        return graph
     }
 }
