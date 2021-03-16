8802d1e53a54048892ac271405ea81fb131e0302
__Wither <jn.beyrand@gmail.com>
2021-03-16 15:55:36 +0100

    Amélioration de prochainChangementEau

--- Appli/app/src/main/java/fr/unilim/iut/aqualala/model/sql/classes/Eau.kt
+++ Appli/app/src/main/java/fr/unilim/iut/aqualala/model/sql/classes/Eau.kt
@@ -13,37 +13,10 @@ class Eau (var derniereDateChangementEau:Date) {
     }
 
     fun prochainChangementEau(frequenceChangementEau:Int) : String{
-        var jour=SimpleDateFormat("dd").format(derniereDateChangementEau).toInt()
-        var mois=SimpleDateFormat("MM").format(derniereDateChangementEau).toInt()
-        var annee=SimpleDateFormat("yyyy").format(derniereDateChangementEau).toInt()
-        var prochainJour = jour+frequenceChangementEau
-        var nombreJourFevrier=28
-        if (annee%4==0) nombreJourFevrier=29
-
-        when (mois){
-            1,3,5,7,8,10,12 -> {  //Les mois avec 3 jours
-                if (prochainJour > 31 ){
-                    mois++
-                    if (mois>12){
-                        mois=1
-                        annee++
-                    }
-                    prochainJour-=31
-                }
-            }
-            4,6,9,11->{
-                if (prochainJour > 30 ){
-                    mois++
-                    prochainJour-=30
-                }
-            }
-            else->{
-                if (prochainJour > nombreJourFevrier ){
-                    mois++
-                    prochainJour-=nombreJourFevrier
-                }
-            }
-        }
-        return "$prochainJour/$mois/$annee"
+        var calendar = Calendar.getInstance() // Création d'u objet calendrier
+        calendar.time = derniereDateChangementEau // attribution de la bonne date au calendrier
+        calendar.add(Calendar.DATE, frequenceChangementEau) // Ajout de frequenceChangementEau en jours
+        var newDate = calendar.time // retour à une date
+        return SimpleDateFormat("dd/MM/yyyy").format(newDate) // Date revoyée formatée
     }
 }
